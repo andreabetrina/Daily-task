519833dd2bd57a6f8d18f37ddf52e3ed6d2040fa
andreabetrina <andreabetrina06@gmail.com>
2025-05-01 11:58:05 +0530

    Create Prime-number.py

---
+++ Prime-number.py
@@ -0,0 +1,11 @@
+#to check whether the give number is Prime or not.
+n = 26
+cnt = 0
+num = n//2
+for i in range ( 2, num ):
+     if (num%i) == 0:
+          cnt+=1
+if cnt ==0:
+     print ( n, " is a Prime  number" )
+else:
+     print( n, " is not a Prime number" )
