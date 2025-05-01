e99b4969941a1d8c24758dc82fc4635682e2d185
Andrea Betrina P <andreabetrina06@gmail.com>
2025-05-02 00:33:01 +0530

    Prime-number.py

--- Prime-number.py
+++ Prime-number.py
@@ -2,10 +2,10 @@
 n = 26
 cnt = 0
 num = n//2
-for i in range ( 2, num ):
-     if (num%i) == 0:
-          cnt+=1
-if cnt ==0:
-     print ( n, " is a Prime  number" )
+for i in range ( 2 , num ):
+    if (num%i) == 0:
+        cnt+=1
+if cnt == 0:
+    print ( n, " is a Prime number" )
 else:
-     print( n, " is not a Prime number" )
+    print( n, " is not a Prime number" )
