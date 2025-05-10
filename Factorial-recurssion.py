734a8715f844143fa2c121c184b7f09a61d57636
Andrea Betrina P <andreabetrina06@gmail.com>
2025-05-10 21:51:49 +0530

    Create Factorial-recurssion.py

---
+++ Factorial-recurssion.py
@@ -0,0 +1,14 @@
+# to find factorial of a number using recursion.
+def factorial (n):
+      if n== 0 or n==1:
+           return 1
+      else:
+           return n* factorial (n-1)
+                
+#main
+n =10
+fact= factorial(n)
+print ( "The Factorial is",fact )
+                
+
+```
