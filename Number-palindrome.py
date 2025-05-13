baa26565f68de95d7d6ab3ee5d2035770b15f837
Andrea Betrina P <andreabetrina06@gmail.com>
2025-05-13 20:54:48 +0530

    Update and rename Fibonacci-series10nos.py to Number-palindrome.py

---
+++ Number-palindrome.py
@@ -0,0 +1,9 @@
+#to whether give number is palindrome or not.
+n=2610
+rev=int(str(n)[::-1])
+t=n
+if (n == rev):
+     print( n ,"is a palindrome.")
+else:
+     print( n,"is not a palindrome.")
+```
