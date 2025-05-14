e1ebb44253b7d45820ea4a26edea11e05fad1300
Andrea Betrina P <andreabetrina06@gmail.com>
2025-05-14 22:39:21 +0530

    Create Swapping-without-temp-variable.py

---
+++ Swapping-without-temp-variable.py
@@ -0,0 +1,10 @@
+#Swapping two numbers without using temporary variable
+a=26
+b=10
+print("Before Swapping")
+print( "a = ",a )
+print( "b = " ,b )
+a,b = b,a
+print("After Swapping")
+print("a =",a )
+print("b =" ,b )
