1137cd37e008afa45988d3806728378a6dfc790f
Andrea Betrina P <andreabetrina06@gmail.com>
2025-05-15 22:20:37 +0530

    Create Largest-3-numbers.py

---
+++ Largest-3-numbers.py
@@ -0,0 +1,10 @@
+#to find largest of 3 numbers.
+a = 349
+b = 261
+c = 888
+if ( a>b ) and ( a>c ):
+     print( a ," is the largest number" )
+elif ( b>a ) and ( b>c ):
+     print( b ," is the largest number" )
+else:
+     print( c , "is the largest number" )
