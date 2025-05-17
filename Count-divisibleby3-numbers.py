d29e098c8493c6d90e83c3984d89c10e93d01a82
Andrea Betrina P <andreabetrina06@gmail.com>
2025-05-17 22:34:13 +0530

    Create Count-divisibleby3-numbers.py

---
+++ Count-divisibleby3-numbers.py
@@ -0,0 +1,7 @@
+# to count how many numbers between 1 and 100 are divisible by 3 and prints the count.
+
+cnt=0
+for i in range( 1 , 101 ):
+     if( i % 3 == 0 ):
+          cnt+=1
+print(cnt , " numbers are divisible by 3 between 1 and 100")
