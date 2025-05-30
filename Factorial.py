dac70aa0b0604db486829fc703a3bae6760bd9ef
Andrea Betrina P <andreabetrina06@gmail.com>
2025-05-30 17:39:30 +0530

    Create Factorial.py

---
+++ Factorial.py
@@ -0,0 +1,6 @@
+# to find the factorial of a number (let’s say 5).
+n = 5
+fact = 1
+for i in range(1, n + 1 ):
+     fact= fact * i
+print("Factorial of 5 =", n ,"×", n-1 ,"×", n-2 ,"×", n-3 ,"×", n-4 ,"=", fact)
